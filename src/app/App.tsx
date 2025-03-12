1e7235541c7868b77b726e7023371988123360dc
Dima <dexymiron@gmail.com>
2025-03-12 14:54:33 +0200

    3: created Navbar component
    -moved links to AppLink component(reusable)
    -added inverted theme color for links

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,16 @@
-import { Link } from "react-router-dom";
 import "./styles/index.scss";
 import { classNames } from "shared/config/lib/classNames";
 import { useTheme } from "app/providers/ThemeProvider";
 import { AppRouter } from "./providers/router";
+import { Navbar } from "widgets/Navbar";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
   return (
     <div className={classNames("app", {}, [theme])}>
-      <button onClick={toggleTheme}>TOGGLE THEME</button>
-      <Link to={"/"}>Главная</Link>
-      <Link to={"/about"}>О странице</Link>
+      <Navbar />
       <AppRouter />
+      <button onClick={toggleTheme}>TOGGLE THEME</button>
     </div>
   );
 };
---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+import { classNames } from "shared/config/lib/classNames";
+import cls from "./Navbar.module.scss";
+import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
+
+interface NavbarProps {
+  className?: string;
+}
+
+export const Navbar = ({ className }: NavbarProps) => {
+  return (
+    <div className={classNames(cls.navbar, {}, [])}>
+      <div className={cls.links}>
+        <AppLink
+          to={"/"}
+          theme={AppLinkTheme.SECONDARY}
+          className={cls.mainLink}
+        >
+          Главная
+        </AppLink>
+        <AppLink to={"/about"} theme={AppLinkTheme.SECONDARY}>
+          О странице
+        </AppLink>
+      </div>
+    </div>
+  );
+};
