0cce28ab5a606e7f7f7a158f059940999da90e37
Dima <dexymiron@gmail.com>
2025-03-24 13:43:01 +0200

    5: added Sidebar widget
    -added basic SideBar component5: added Sidebar
    widget
    
    -added basic SideBar component5: added Sidebar
    widget
    
    -added basic SideBar component5: added Sidebar
    widget
    
    -added basic SideBar component5: added Sidebar
    widget
    
    -added basic SideBar component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,13 +3,17 @@ import { classNames } from "shared/config/lib/classNames";
 import { useTheme } from "app/providers/ThemeProvider";
 import { AppRouter } from "./providers/router";
 import { Navbar } from "widgets/Navbar";
+import { Sidebar } from "widgets/SideBar";
 
 const App = () => {
   const { theme } = useTheme();
   return (
     <div className={classNames("app", {}, [theme])}>
       <Navbar />
-      <AppRouter />
+      <div className="content-page">
+        <Sidebar />
+        <AppRouter />
+      </div>
     </div>
   );
 };
