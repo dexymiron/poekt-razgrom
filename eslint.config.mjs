61629bf4c9e61c44f5c082a3205c49cde958c26b
Dima <dexymiron@gmail.com>
2025-03-28 17:06:08 +0200

    8: added ESlint

---
+++ eslint.config.mjs
@@ -0,0 +1,40 @@
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    plugins: {
+      js,
+    },
+    extends: ["js/recommended"],
+  },
+  {
+    ...tseslint.configs.recommended,
+  },
+  {
+    ...pluginReact.configs.flat.recommended,
+    rules: {
+      ...pluginReact.configs.flat.recommended.rules,
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-indent": ["error", 4],
+      "react/jsx-filename-extension": [
+        1,
+        { extensions: [".js", ".jsx", ".tsx"] },
+      ],
+      "import/no-unresolved": "off",
+      "no-unused-vars": "warn",
+      indent: [2, 4],
+      "react/require-default-props": "off",
+      "react/jsx-props-no-spreading": "warn",
+      "react/function-component-definition": "off",
+      "no-shadow": "off",
+    },
+  },
+]);
